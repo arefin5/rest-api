798867de58868ced362d13a79c66809c61c67264
arefin5 <arefintalukder5@gmail.com>
2024-09-15 03:35:24 +0600

    delete list update list midleware

--- router/hostRoute.js
+++ router/hostRoute.js
@@ -1,16 +1,21 @@
 // const express = require("express")
-const { requireAuth } = require("../midleware/auth");
 const express = require("express");
 const router = express.Router();
+const { requireAuth,requireSignin ,canEditDeletePost} = require("../midleware/auth");
 
 const {
   createList,
-  lists
+  lists,
+  updateList,
+  deleteSinglelist,
+  getSingleList
   
 } = require("../controlar/list.js");
 
 
-router.post('/create-list',requireAuth,createList);
-router.get('/all-list',requireAuth,lists);
-
+router.post('/create-list',requireSignin,createList);
+router.get('/all-list',requireSignin,lists);
+router.get("/get-single-list/:id",requireSignin,getSingleList)
+router.put("/update-list/:id",requireSignin,canEditDeletePost,updateList)
+router.delete("/delete-list/:id",requireSignin,canEditDeletePost,deleteSinglelist)
 module.exports = router;
