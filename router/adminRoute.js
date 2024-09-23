41bc91943637c58e7996106123cd0b4e2cfeb970
arefin5 <arefintalukder5@gmail.com>
2024-09-23 20:54:38 +0600

    admin and host route user block get all user get all booking

--- router/adminRoute.js
+++ router/adminRoute.js
@@ -16,17 +16,18 @@ const {
     blockuser,
     aprovedListPublished
 } = require("../controlar/admin.js");
-router.get('/failed-booking-all',requireSignin,checkAdmin,getFailedBookings);
-router.get('/all-booking',requireSignin,checkAdmin,getAllBooking);
-router.get("/get-single-list/:id",requireSignin,checkAdmin,getAllPanding);
-router.put("/published-list/:id",requireSignin,checkAdmin,aprovedList);
-router.put("/booking-confirm/:id",requireSignin,checkAdmin,aprovedBooking);
-router.put("/create-admin/:id",requireSignin,checkAdmin,aprovedAdmin)
-router.put("/published-from-host",canEditDeletePost,aprovedListPublished)
-router.get("/all-user",requireSignin,checkAdmin,getAlluser);
-router.get("/all-list",requireSignin,checkAdmin,getAllList);
+router.get('/admin/failed-booking-all',requireSignin,checkAdmin,getFailedBookings);
+router.get('/admin/all-booking',requireSignin,checkAdmin,getAllBooking);
+router.put("/admin/published-list/:id",requireSignin,checkAdmin,aprovedList);
+router.put("/admin/booking-confirm/:id",requireSignin,checkAdmin,aprovedBooking);
+router.put("/admin/create-admin/:id",requireSignin,checkAdmin,aprovedAdmin)
+router.get("/admin/all-user",requireSignin,checkAdmin,getAlluser);
+router.get("/admin/all-list",requireSignin,checkAdmin,getAllList);
+
+router.get("/admin/get-pending/",requireSignin,checkAdmin,getAllPanding);
 
 router.put("/block-user/:id",requireSignin,checkAdmin,blockuser);
+router.put("/admin/published-from-host/:id",canEditDeletePost,aprovedListPublished)
 
 
 module.exports = router;
