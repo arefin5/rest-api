d9cd6c8d0fa9421c7693a60a89411a6309529db4
arefin5 <arefintalukder5@gmail.com>
2024-09-27 15:50:30 +0600

    change port

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/mongodb-data', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
