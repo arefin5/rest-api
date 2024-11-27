996e28e4f0635eae42f96033f966d1a3c0c3ead8
arefin5 <arefintalukder5@gmail.com>
2024-11-28 04:54:36 +0600

    gogle login with mongdb working

--- passport.js
+++ passport.js
@@ -1,22 +1,44 @@
-const passport = require('passport'); 
-const GoogleStrategy = require('passport-google-oauth2').Strategy; 
-
-passport.serializeUser((user , done) => { 
-	done(null , user); 
-}) 
-passport.deserializeUser(function(user, done) { 
-	done(null, user); 
-}); 
-
-passport.use(new GoogleStrategy({ 
-	clientID:process.env.CLIENT_ID, // Your Credentials here. 
-	clientSecret:process.env.CLIENT_SECRET, // Your Credentials here. 
-	callbackURL:"http://localhost:3000/auth/google/callback", 
-	passReqToCallback:true
-}, 
-function(request, accessToken, refreshToken, profile, done) { 
-	return done(null, profile); 
-} 
-));
+require('dotenv').config();
 
 
+
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+module.exports = (passport) => {
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
+
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/callback",
+        passReqToCallback: true,
+		scope: ["email", "profile"]
+      },
+      (request, accessToken, refreshToken, profile, done) => {
+        return done(null, profile);
+      }
+    )
+  );
+// passport.use(
+// 	new GoogleStrategy(
+// 	  {
+// 		clientID: process.env.CLIENT_ID,
+// 		clientSecret: process.env.CLIENT_SECRET,
+// 		callbackURL: "http://localhost:3000/login/sociallogin/callback",
+// 		passReqToCallback: true,
+// 	  },
+// 	  (request, accessToken, refreshToken, profile, done) => {
+// 		console.log("User Profile:", profile);
+// 		return done(null, profile);
+// 	  }
+// 	)
+//   );
+};
